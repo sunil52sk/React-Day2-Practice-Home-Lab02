b989cd98bb76b4723c8012507161ef50daeb4e46
sunilkumar_seenivasan <100457242+sunil52sk@users.noreply.github.com>
2023-03-28 09:27:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Mybutton from './Component/Mybutton';
 function App() {
   return (
      <div className='App'>
-      <Mybutton Text="Button 1" num="1"/>
-      <Mybutton Text="Button 2" num="2"/>
-      <Mybutton Text="Button 3" num="3"/>
+      <Mybutton Text="Button 1"/>
+      <Mybutton Text="Button 2"/>
+      <Mybutton Text="Button 3"/>
      </div>
   );
 }
